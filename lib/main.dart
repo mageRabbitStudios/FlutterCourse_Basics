5deeccc551325ea82303c7ca65fbaf39cd00c994
Stanislav Kinzl <kinzlstanislav@gymnachod.cz>
2018-12-18 15:20:33 +0000

    [Basics] before products.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,50 @@ void main() {
   runApp(DemoApp());
 }
 
-class DemoApp extends StatelessWidget {
+class DemoApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    //create state
+    return _DemoAppState();
+  }
+}
+
+class _DemoAppState extends State<DemoApp> {
+
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Demo App'),
+          title: Text('Flutter Basics App'),
         ),
-      ), //home of the widget
+        body: Column(children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              child: Text('Add Product'),
+              onPressed: () {
+                setState(() {
+                  _products.add('Advanced Food Tester');
+                });
+              },
+            ),
+          ),
+          Column(children: _products
+              .map((element) => Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food.jpg'),
+                    Text(element)
+                  ],
+                ),
+              )
+            ).toList()
+          ),
+        ]),
+      ),
     );
   }
 }
